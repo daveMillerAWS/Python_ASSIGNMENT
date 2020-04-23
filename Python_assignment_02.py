d462b9c399120a78536caf0abbde1fa7b0887fa4
Dave Miller <miller.dave0219@gmail.com>
2020-04-23 13:19:46 -0500

    This is second assignment

---
+++ Python_assignment_02.py
@@ -0,0 +1,25 @@
+'''
+
+Problem :
+
+    Task : Estimating the risk of death from coronavirus. Write a program that;
+    Takes "Yes" or "No" from the user as an answer to the following questions :
+        Are you a cigarette addict older than 75 years old? Variable → age
+        Do you have a severe chronic disease? Variable → chronic
+        Is your immune system too weak? Variable → immune
+
+Set a logical algorithm using boolean logic operators (and/or) and
+use if-statements with the given variables in order to
+ print out us a message : "You are in risky group"(if True )
+  or "You are not in risky group" (if False).
+
+'''
+age = input("Are you a cigarette addict older than 75 years old? [y/n]")
+chronic = input("Do you have a severe chronic disease? [y/n]")
+immune = input("Is your immune system too weak? [y/n]")
+if 'y' in (age,chronic,immune):
+    print("You are in risky group")
+else:
+    print("You are not in risky group")
+
+
