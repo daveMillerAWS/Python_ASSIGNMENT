70efdec74e095cf52a210e0e0d3bfae5d533be95
Dave Miller <miller.dave0219@gmail.com>
2020-04-23 11:30:32 -0500

    This is a python assignment

---
+++ Python_assignment_01.py
@@ -0,0 +1,11 @@
+'''
+Write a program that
+
+    Takes the first name from the user and compares it to yours,
+    Then if the name the user entered is the same as yours, print out such as : "Hello, Joseph! The password is : @12",
+    If the name the user entered is not the same as yours, print out such as : "Hello, Amina! See you later."
+'''
+if input("Your name: ") == 'Joseph':
+    print("Hello, Joseph! The password is : @12")
+else:
+    print("Hello, Amina! See you later.")
