5cf12553daf919091a0743bd35b79a44dcb7a230
Dave Miller <miller.dave0219@gmail.com>
2020-04-29 11:32:58 -0500

    This is 4th python assignment by Dave Miller

---
+++ Python_Assignment04.py
@@ -0,0 +1,31 @@
+'''
+
+Task : Write a program that takes a number from the user and prints the result to check if it is a prime number.
+
+The examples of the desired output are as follows :
+
+input →  19 ⇉ output : 19 is a prime number
+input →  10 ⇉ output : 10 is not a prime number
+
+'''
+
+while True:
+    result = "is not a prime number"
+# INPUT
+    num = input("Enter a number: ")
+# IS PRIME ?
+    if num.isdecimal():
+        num = int(num)
+        if num != 0 and num != 1:
+            if num == 2:
+                result = "is a prime number"
+            else:
+                for divider in range(2,num):
+                    print(divider)
+                    if num % divider == 0:
+                        print("break")
+                        break
+                    result = "is a prime number"
+    else:
+        print("Invalid Enterance!!! retry ")
+    print(num,result)
